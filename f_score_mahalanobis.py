e32593f6a5cf0837764f39a5d4a7455165c51b1e
Vyas Raina <sayvaniar@gmail.com>
2021-11-11 14:32:35 +0000

    pr curves

---
+++ f_score_mahalanobis.py
@@ -0,0 +1,123 @@
+'''
+Expected a tensor of logits at the input: C x [N x 5 x 2 x 14]
+
+    List Dim: Class
+            [C] # 14 - label
+
+    Dim 0: Data point
+            [N] # Number of successful adversarial attacks for class
+
+    Dim 1: Seed
+            [5] # 5 models in ensemble
+
+    Dim 2: Original or Adversarial
+            [2]
+
+    Dim 3: Emotion Logit predictions
+            [14] # 14 classes
+
+
+Use the Mahalanobis distance in the logit space as a metric for
+each original and adv sample.
+
+Calculate the best F1-score for this metric to detect
+adversarial samples.
+'''
+
+import numpy as np
+import sys
+import os
+import argparse
+from sklearn.metrics import precision_recall_curve
+import matplotlib.pyplot as plt
+from f_score_uncertainty_all import get_best_f_score
+
+
+def calculate_per_class_dist(vector, class_mean, inv_cov):
+    diff = vector - class_mean
+    # print(diff)
+    half = np.matmul(inv_cov, diff)
+    return np.dot(diff, half)
+
+def calculate_mahalanobis(vector, class_means, inv_cov):
+    # Select closest class conditional distance
+    dists = []
+    for class_mean in class_means:
+        dists.append(calculate_per_class_dist(vector, class_mean, inv_cov))
+    return min(dists)
+
+
+if __name__ == '__main__':
+    # Get command line arguments
+    commandLineParser = argparse.ArgumentParser()
+    commandLineParser.add_argument('FILENAME', type=str, help='.npz file with list of logits array')
+    commandLineParser.add_argument('OUT', type=str, help='.png file for pr curve')
+
+    args = commandLineParser.parse_args()
+    filename = args.FILENAME
+    out_file = args.OUT
+
+    NUM_CLASSES = 14
+
+    # Save the command run
+    if not os.path.isdir('CMDs'):
+        os.mkdir('CMDs')
+    with open('CMDs/f_score_mahalanobis.cmd', 'a') as f:
+        f.write(' '.join(sys.argv)+'\n')
+
+    # Load the logits array list
+    # Keep only seed 1 logits (only model used for detector in logit space)
+    logits_dict = np.load(filename)
+    train_original_logits_list = []
+    test_original_logits_list = []
+    test_adv_logits_list = []
+    FRAC = 0.8
+    for i in range(NUM_CLASSES):
+        logits = np.squeeze(logits_dict[f'arr_{i}'][:,0,:,:])
+        train_logits = logits[:int(FRAC*len(logits)),:,:]
+        test_logits = logits[int(FRAC*len(logits)):,:,:]
+
+        train_original_logits_list.append(np.squeeze(train_logits[:,0,:]))
+        test_original_logits_list.append(np.squeeze(test_logits[:,0,:]))
+        test_adv_logits_list.append(np.squeeze(test_logits[:,1,:]))
+
+     # Calculate class specific means
+     # Calculate an averaged tied covariance matrix
+    class_means = []
+    cov = np.zeros((NUM_CLASSES, NUM_CLASSES))
+    for i in range(NUM_CLASSES):
+        class_mean = np.mean(train_original_logits_list[i], axis=0)
+        class_means.append(class_mean)
+
+        class_cov = np.cov(train_original_logits_list[i], rowvar=False)
+        cov += class_cov
+    cov = cov/NUM_CLASSES
+
+    # Calculate Mahalanobis distances per test data point
+    inv_cov = np.linalg.inv(cov)
+
+    original_dists = []
+    for logits in np.concatenate(test_original_logits_list):
+        original_dists.append(calculate_mahalanobis(logits, class_means, inv_cov))
+
+    adv_dists = []
+    for logits in np.concatenate(test_adv_logits_list):
+        adv_dists.append(calculate_mahalanobis(logits, class_means, inv_cov))
+
+    # print(original_dists)
+    # print(adv_dists)
+
+    dists = np.asarray(original_dists+adv_dists)
+    labels = [0]*len(original_dists) + [1]*len(adv_dists)
+
+    # Calculate best F1-score
+    precision, recall, _ = precision_recall_curve(labels, dists)
+    best_precision, best_recall, best_f1 =  get_best_f_score(precision, recall)
+
+    # plot all the data
+    plt.plot(recall, precision, 'r-')
+    plt.plot(best_recall,best_precision,'bo')
+    plt.annotate(f"F1={best_f1:.2f}", (best_recall, best_precision))
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.savefig(out_file)
---
+++ f_score_uncertainty_all.py
@@ -0,0 +1,111 @@
+'''
+Identical to f_score_uncertainty, but generalized for many
+different classification uncertainty measures:
+
+- Expected Pair-Wise KL Divergence (epkl)
+- entropy of expected
+- expected entropy
+- mutual information
+- reverse mutual information
+- negative confidence
+
+Expected a tensor of logits at the input: [N x 5 x 2 x 14]
+
+    Dim 0: Data point
+            [N] # Number of successful adversarial attacks
+
+    Dim 1: Seed
+            [5] # 5 models in ensemble
+
+    Dim 2: Original or Adversarial
+            [2]
+
+    Dim 3: Emotion Logit predictions
+            [14] # 14 classes
+'''
+
+import sys
+import os
+import argparse
+import numpy as np
+from scipy.special import softmax
+from sklearn.metrics import precision_recall_curve
+import matplotlib.pyplot as plt
+from uncertainty import ensemble_uncertainties_classification
+
+def get_best_f_score(precisions, recalls, beta=1.0):
+    f_scores = (1+beta**2)*((precisions*recalls)/((precisions*(beta**2))+recalls))
+    ind = np.argmax(f_scores)
+    return precisions[ind], recalls[ind], f_scores[ind]
+
+if __name__ == '__main__':
+	# Get command line arguments
+    commandLineParser = argparse.ArgumentParser()
+    commandLineParser.add_argument('FILENAME', type=str, help='.npy file with logits array')
+    commandLineParser.add_argument('OUT_DIR', type=str, help='directory to save .png files for pr curves to')
+
+    args = commandLineParser.parse_args()
+    filename = args.FILENAME
+    out_dir = args.OUT_DIR
+
+    # Save the command run
+    if not os.path.isdir('CMDs'):
+        os.mkdir('CMDs')
+    with open('CMDs/f_score_uncertainty_all.cmd', 'a') as f:
+        f.write(' '.join(sys.argv)+'\n')
+    
+    # Load the logits array
+    logits = np.load(filename)
+    probs = softmax(logits, axis=-1)
+    print("Array shape", probs.shape)
+
+    original_probs = probs[:,:,0,:].squeeze()
+    adv_probs = probs[:,:,1,:].squeeze()
+
+    # Transpose to match uncertainty function format
+    original_probsT = np.transpose(original_probs, (1, 0, 2))
+    adv_probsT = np.transpose(adv_probs, (1, 0, 2))
+
+    # Get uncertainty measure for each data point
+    original_uncertainties = ensemble_uncertainties_classification(original_probsT)
+    adv_uncertainties = ensemble_uncertainties_classification(adv_probsT)
+
+    # For each uncertainty measure get separate PR curve
+    print(len(original_probs), len(adv_probs))
+    labels = [0]*len(original_probs) + [1]*len(adv_probs)
+
+    for measure in original_uncertainties.keys():
+        original = original_uncertainties[measure]
+        adv = adv_uncertainties[measure]
+        together = np.concatenate((original, adv))
+
+        # Calculate best F1-score
+        precision, recall, _ = precision_recall_curve(labels, together)
+        best_precision, best_recall, best_f1 =  get_best_f_score(precision, recall)
+
+        # plot all the data
+        out_file = f'{out_dir}/pr_{measure}.png'
+        plt.plot(recall, precision, 'r-')
+        plt.plot(best_recall,best_precision,'bo')
+        plt.annotate(F"F1={best_f1:.2f}", (best_recall,best_precision))
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.savefig(out_file)
+        plt.clf()
+
+    # Plot all uncertainty measure PR curves on same graph
+    out_file = f'{out_dir}/pr_all_uncertainty.png'
+    for measure in original_uncertainties.keys():
+        original = original_uncertainties[measure]
+        adv = adv_uncertainties[measure]
+        together = np.concatenate((original, adv))
+
+        # Calculate best F1-score
+        precision, recall, _ = precision_recall_curve(labels, together)
+        best_precision, best_recall, best_f1 =  get_best_f_score(precision, recall)
+        plt.plot(recall, precision, label=f'{measure}, F1={best_f1:.2f}')
+        plt.plot(best_recall,best_precision,'ko')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.legend()
+    plt.savefig(out_file)
